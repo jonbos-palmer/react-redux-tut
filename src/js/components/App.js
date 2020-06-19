a336085763328702f0c7e6e8839ce04fd4a4e30b
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-19 17:00:27 -0500

    add Post component...not sure exactly whats happening

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import List from "./List";
 import Form from "./Form";
+import Post from "./Post";
 const App = () => (
   <>
     <div>
@@ -10,6 +11,10 @@ const App = () => (
     <div>
       <h2>Add a New Article</h2>
       <Form />
+    </div>
+    <div>
+      <h2>API posts</h2>
+      <Post />
     </div>
   </>
 );
