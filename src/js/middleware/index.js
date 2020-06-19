cc03e5321dcae71720a61b026bf4f050d3441566
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-19 15:54:45 -0500

    fix: export function

--- src/js/middleware/index.js
+++ src/js/middleware/index.js
@@ -1,7 +1,7 @@
 import { ADD_ARTICLE } from "../constants/action-types";
 const forbiddenWords = ["spam", "money"];
 
-function forbiddenWordsMiddleware({ dispatch }) {
+export function forbiddenWordsMiddleware({ dispatch }) {
   return function (next) {
     return function (action) {
       if (action.type === ADD_ARTICLE) {
