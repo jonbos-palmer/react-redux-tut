75f7bbd99943ff1e6f4a9438892b44433b8e1059
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-19 11:22:42 -0500

    wire form component into app

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import List from "./List";
+import Form from "./Form";
 const App = () => (
-  <div>
-    <h2>Articles</h2>
-    <List />
-  </div>
+  <>
+    <div>
+      <h2>Articles</h2>
+      <List />
+    </div>
+    <div>
+      <h2>Add a New Article</h2>
+      <Form />
+    </div>
+  </>
 );
+
+export default App;
